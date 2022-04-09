7f962ccdd960504de89ab74edc6d26756f112c65
FilipPP8 <fparchanowicz@gmail.com>
2022-04-09 11:48:15 +0200

    Implements Player Movement and Player Collision (also added Player Prefab and Collision Layers)

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] InputManager _inputManager;
+
+    [SerializeField] Rigidbody2D _rigidBody;
+    [SerializeField] float _speed;
+
+    Camera _activeCamera;
+    Rect _cameraBounds;
+
+    private void Start() 
+    {
+        _activeCamera = Camera.main;
+
+        Vector3 bottomLeftPosition = _activeCamera.ScreenToWorldPoint(Vector3.zero);
+        Vector3 topRightPosition = _activeCamera.ScreenToWorldPoint (
+            new Vector3(_activeCamera.pixelWidth, _activeCamera.pixelHeight));
+
+        _cameraBounds = new Rect(bottomLeftPosition.x, bottomLeftPosition.y, 
+        topRightPosition.x - bottomLeftPosition.x, topRightPosition.y - bottomLeftPosition.y);  
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector2 _movementVector = new Vector2 
+        (_inputManager.HorizontalInput * _speed, _inputManager.VerticalInput * _speed);
+
+        _rigidBody.AddForce(_movementVector);
+    }
+
+    private void LateUpdate() 
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, _cameraBounds.xMin, _cameraBounds.xMax),
+            Mathf.Clamp(transform.position.y, _cameraBounds.yMin, _cameraBounds.yMax),
+            transform.position.z
+        );    
+
+
+    }
+
+}
